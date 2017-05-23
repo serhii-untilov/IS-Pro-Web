88f5e96042a79bf5e988659b0586676e8795ef4b
uncle zeppelin <uncle.zeppelin@gmail.com>
2017-05-23 22:41:06 +0300

    password md5

--- src/main/java/ua/in/usv/stay/PasswordBlock.java
+++ src/main/java/ua/in/usv/stay/PasswordBlock.java
@@ -3,6 +3,8 @@ package ua.in.usv.stay;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Arrays;
+
 @Getter
 @Setter
 public class PasswordBlock {
@@ -15,13 +17,34 @@ public class PasswordBlock {
     private int salt;
     private byte[] hash;
 
-    public PasswordBlock(byte[] block){
+    public PasswordBlock(byte[] block) {
         version = block[0];
         format = block[1];
         unused1 = 0;
-        salt = (int) (((int)(block[4]) << 24) | ((int)(block[5]) << 16) | ((int)(block[6]) << 8) | ((int)(block[7])));
+
+        salt  = (int)(block[4] & 0xFF) << 24;
+        salt |= (int)(block[5] & 0xFF) << 16;
+        salt |= (int)(block[6] & 0xFF) << 8;
+        salt |= (int)(block[7] & 0xFF);
+
         byte[] hash = new byte[block.length - 8];
-        System.arraycopy(hash, 0, block, 8, block.length - 8);
+        System.arraycopy(block, 8, hash, 0, block.length - 8);
         this.hash = hash;
+    }
+
+    @Override
+    public String toString() {
+        if (hash == null)
+            return "";
+        int iMax = hash.length - 1;
+        if (iMax == -1)
+            return "";
+        StringBuilder b = new StringBuilder();
+        for (int i = 0; ; i++) {
+            int charCode = hash[i] & 0xFF;
+            b.append(Character.toString((char)charCode));
+            if (i == iMax)
+                return b.toString();
+        }
     }
 }
--- src/test/java/ua/in/usv/D4ApplicationTests.java
+++ src/test/java/ua/in/usv/D4ApplicationTests.java
@@ -2,8 +2,12 @@ package ua.in.usv;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 import static junit.framework.TestCase.assertTrue;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,6 +21,8 @@ import ua.in.usv.stay.PasswordBlock;
 @SpringBootTest
 public class D4ApplicationTests {
 
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	@Autowired
 	private UserService userService;
 
@@ -29,6 +35,16 @@ public class D4ApplicationTests {
 	public void passwordHashRead(){
 		CustomUser user = userService.findByLogin("usv");
 		PasswordBlock passwordBlock = new PasswordBlock(user.getUserPassword().getPasswordBlob());
-		assertThat(passwordBlock.getHash().length == PasswordBlock.digest_len);
+		assertTrue(passwordBlock.getHash().length == PasswordBlock.digest_len);
+	}
+
+	@Test
+	public void md5PasswordHashCompare(){
+		CustomUser user = userService.findByLogin("usv");
+		PasswordBlock passwordBlock = new PasswordBlock(user.getUserPassword().getPasswordBlob());
+		Md5PasswordEncoder md = new Md5PasswordEncoder();
+		Object salt = passwordBlock.getSalt();
+		String pass = md.encodePassword("", salt);
+		assertTrue(md.isPasswordValid(passwordBlock.toString(), "", salt));
 	}
 }
