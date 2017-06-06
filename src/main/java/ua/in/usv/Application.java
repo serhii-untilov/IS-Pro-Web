023a0c09575fb800a3b4daa4d4f56be42190aea5
unclezeppelin <uncle.zeppelin@gmail.com>
2017-06-06 16:13:10 +0300

    login page localization

--- src/main/java/ua/in/usv/Application.java
+++ src/main/java/ua/in/usv/Application.java
@@ -3,11 +3,15 @@ package ua.in.usv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import sun.util.locale.BaseLocale;
+import sun.util.locale.InternalLocaleBuilder;
 
 import java.util.Locale;
 
@@ -20,7 +24,8 @@ public class Application extends WebMvcConfigurerAdapter {
     @Bean
     public LocaleResolver localeResolver() {
         SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(Locale.US);
+        Locale defaultLocale = new Locale("uk");
+        slr.setDefaultLocale(defaultLocale);
         return slr;
     }
 
